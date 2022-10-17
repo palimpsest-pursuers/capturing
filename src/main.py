bcc2cda57601a7bd3ac08ce296fd79e933a6566c
Mal <mmb2582@g.rit.edu>
2022-10-17 15:53:52 -0400

    it turns on

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import sys, os
-from PyQt5 import uic, QtWidgets
+from PyQt5 import uic, QtWidgets, QtCore, QtGui
 
 print("START")
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QMainWindow()
 
     ui_path = os.path.join(os.path.dirname(__file__), 'skeleton/capture-mode2.ui')
     uic.loadUi(ui_path, window)
+    
+    TestLedsButton = QtWidgets.QPushButton('TestLedsButton')
     
     window.show()
     sys.exit(app.exec_())
--- src/operations/led_control.py
+++ src/operations/led_control.py
@@ -3,11 +3,10 @@ import serial
 class LEDController:
 
     #wavelengths for the LEDs on the boards in nanometers (nm)
-    wavelength_list = [356, 385, 395, 420,
-                       450, 470, 490, 520, 
-                       560, 590, 615, 630, 
-                       660, 730, 850, 940]
-
+    wavelength_list = ['356', '385', '395', '420',
+                       '450', '470', '490', '520', 
+                       '560', '590', '615', '630', 
+                       '660', '730', '850', '940']
     def __init__(self):
         """Open up the connection to LED's serial port"""
         self.led_connection = serial.Serial('COM3', 9600)
@@ -23,13 +22,21 @@ class LEDController:
         """
         Turn on a specific LED using wavelength
         The LED board accepts commands written as:
-            'wavelength,intensity\n'
+            'wavelength,intensity\\n'
         For our purposes, the intensity will always be 100
         """
-        self.led_connection.write(wavelength + ',100\n')
+        command = (wavelength + ',100\n')
+        print(command)
+        print(command.encode())
+        self.led_connection.write((wavelength + ',100\n').encode())
 
     def turn_off(self):
         """Turn off all LEDs"""
-        self.led_connection.write('0,0\n')
+        self.led_connection.write('0,0\n').encode()
 
-    
+
+if __name__ == '__main__':
+    lc = LEDController()
+    print(lc.led_connection.isOpen())
+    lc.turn_on('630')
+    lc.turn_off()
