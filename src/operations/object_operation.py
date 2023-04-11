d6d83072017f38c020fa13a0a25be8a5c8b5cfd3
ram7912 <ram7912@rit.edu>
2023-04-10 20:07:09 -0400

    histogram
    Signed-off-by: ram7912 <ram7912@rit.edu>

--- src/operations/object_operation.py
+++ src/operations/object_operation.py
@@ -38,16 +38,30 @@ class ObjectOp(Operation):
 
     def updateFrame(self, img):
         scene = QtWidgets.QGraphicsScene()
-        scene.addPixmap(img.scaled(self.main.objectstep1View.width(), self.main.objectstep1View.height(), QtCore.Qt.KeepAspectRatio))
+        scene.addPixmap(img.scaled(self.main.objectstep1View.width()-14, self.main.objectstep1View.height() -14, QtCore.Qt.KeepAspectRatio))
         self.main.objectstep1View.setScene(scene)
 
     def updateZoomed(self, img):
         scene = QtWidgets.QGraphicsScene()
-        scene.addPixmap(img.scaled(self.main.objectStep1Zoom.width(), self.main.objectStep1Zoom.height(), QtCore.Qt.KeepAspectRatio))
+        scene.addPixmap(img.scaled(self.main.objectStep1Zoom.width()-14, self.main.objectStep1Zoom.height()-14, QtCore.Qt.KeepAspectRatio))
         self.main.objectStep1Zoom.setScene(scene)
 
     def updateHistogram(self, hist):
-        pass
+        scene = QtWidgets.QGraphicsScene()
+
+        # Determine the width and height of the scene
+        width = self.main.objectStep1Hist.width() - 14
+        height =  self.main.objectStep1Hist.height() - 14
+
+        # Create a QGraphicsRectItem object for each histogram bar
+        bar_width = width / len(hist)
+        max_height = max(hist)
+        for i, value in enumerate(hist):
+            bar_height = height * value / max_height
+            bar = QtWidgets.QGraphicsRectItem(i * bar_width, height - bar_height, bar_width, bar_height)
+            scene.addItem(bar)
+
+        self.main.objectStep1Hist.setScene(scene)
 
     def updateWavelength(self, wavelength):
         self.main.objectStep1Wave.setText("Wavelength: " + wavelength)
@@ -81,8 +95,8 @@ class CaptureWorker(QObject):
             img = self.main.camera_control.convert_nparray_to_QPixmap(frame)
             self.sharedFrame.emit(img)
             self.main.led_control.turn_off()
-            #histogram = np.histogram(frame)
-            #self.histogram.emit(frame)
+            histogram, bins = np.histogram(frame, bins=20, range=(0, 255))  # use 20 bins and a range of 0-255
+            self.histogram.emit(histogram)
             
             zoom = self.main.camera_control.zoom(frame,float(4.0))
             zImg = self.main.camera_control.convert_nparray_to_QPixmap(zoom)
