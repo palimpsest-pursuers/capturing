9534c68d49d0958dda6353e7d3b4a81bf9cf2d6d
ram7912 <ram7912@rit.edu>
2023-04-11 16:15:35 -0400

    fix again
    Signed-off-by: ram7912 <ram7912@rit.edu>

--- src/operations/edit_operation.py
+++ src/operations/edit_operation.py
@@ -25,7 +25,6 @@ class EditOp(Operation):
         pass
 
 
-
     def auto_calibrate(self):
         import numpy as np
         from PIL import Image, ImageOps
@@ -33,17 +32,17 @@ class EditOp(Operation):
 
         # Define filter size
         filter_size = (20, 20)
-        
+
         # Define threshold increment and maximum number of iterations
         thresh_increment = 0.05
         max_iterations = 20
-        
+
         # Get the image array from self
         img_array = self.main.cube_builder.img_array
-        
+
         # Get the number of images and image dimensions
         num_images, height, width = img_array.shape
-        
+
         # Initialize filtered image array
         img_array_filt = np.zeros_like(img_array)
 
@@ -53,39 +52,42 @@ class EditOp(Operation):
         progress.setMinimumDuration(0)
         progress.setAutoReset(True)
         progress.show()
-        
+
         # Process images in batches
         batch_size = 10
-        for i in range(0, num_images, batch_size):
+        num_batches = int(np.ceil(num_images / batch_size))
+        for i in range(num_batches):
             # Get batch of images
-            img_batch = img_array[i:i+batch_size]
+            start_idx = i * batch_size
+            end_idx = min(start_idx + batch_size, num_images)
+            img_batch = img_array[start_idx:end_idx]
 
             # Filter batch of images
             img_batch_filt = np.zeros_like(img_batch)
             for j in range(len(img_batch)):
-                img_batch_filt[j] = ndimage.convolve(img_batch[j], np.ones(filter_size)/(filter_size[0]*filter_size[1]), mode='reflect')
-            
+                img_batch_filt[j] = ndimage.convolve(img_batch[j], np.ones(filter_size) / (filter_size[0] * filter_size[1]), mode='reflect')
+
             # Rescale batch of images
             p1 = np.percentile(img_batch_filt, 1)
             p99 = np.percentile(img_batch_filt, 99)
             img_batch_filt = (img_batch_filt - p1) / (p99 - p1)
-            
+
             # Add filtered batch of images to filtered image array
-            img_array_filt[i:i+batch_size] = img_batch_filt
-            
+            img_array_filt[start_idx:end_idx] = img_batch_filt
+
             # Update progress bar
-            progress.setValue(i+batch_size)
+            progress.setValue(end_idx)
             QtWidgets.QApplication.processEvents()
-            
+
             # Check for cancel
             if progress.wasCanceled():
                 break
-        
+
         # Hide progress bar
         progress.hide()
 
         # Display the 11th image in the filtered image array as an example
-        img = img_array_filt[:,:,11]
+        img = img_array_filt[:, :, 11]
         img = ((img - np.min(img)) / (np.max(img) - np.min(img))) * 255
         img = img.astype(np.uint8)
         qimg = QtGui.QImage(img.data, img.shape[1], img.shape[0], img.strides[0], QtGui.QImage.Format_Grayscale8)
@@ -97,6 +99,7 @@ class EditOp(Operation):
 
         # Replace the original image array with the filtered image array in self
         self.main.cube_builder.img_array = img_array_filt
+
 
     def finished(self):
         pass
