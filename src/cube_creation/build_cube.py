c143bc11dd63f4c9cf5ad67b97e4964ff956e7d8
Mal <mmb2582@g.rit.edu>
2023-04-18 00:50:22 -0400

    this might have the right calibration math

--- src/cube_creation/build_cube.py
+++ src/cube_creation/build_cube.py
@@ -90,6 +90,40 @@ class CubeBuilder():
         dataCube[dataCube > 1] = 1
         self.final_cube = dataCube
 
+        """
+        //%temps = double(dataCube).*double(binaryImage_cube);
+        //I just picked float32 because I know that's what processing likes for cubes
+        temps = self.final_array.astype(np.float32) * binaryImage
+
+
+        //count all the pixels in binary image where binary image is not 0
+        //I don't think we need to sum twice because that where clause should
+        //just count the 1s (we could use binaryImage == 1). 
+        //If not, use the sum based on axis like I have below for meantemp
+        //%cnt2 = sum(sum(binaryImage(binaryImage ~= 0)));
+        ones_count = np.sum(binaryImage, where=(binaryImage != 0))
+
+        //sum up all the pixel values under the mask for each array, divide by total number of ones
+        //to get the mean pixel value under the mask for each band                        
+        //%meantemp = sum(sum(temps))/cnt2;
+        sum_columns = np.sum(temps, axis=0)
+        sum_rows = np.sum(temps, axis=1)
+        meantemp = sum_rows / ones_count
+
+        //take that mean value for each band and repeat it to make it the 
+        //size of the original image                        
+        //%meantemp_cube = repmat(meantemp,[size(dataCube,1) size(dataCube,2),1]);
+        meantemp_cube = np.repeat(meantemp, (self.final_array.shape[0], self.final_array.shape[1], 0)
+
+                        
+        //%calibrate data - divide by mean spectralon per band  
+        //Combining these two because it doesn't look too bad                   
+        //%dataCube = double(dataCube)./meantemp_cube;
+        //%dataCube(dataCube > 1) = 1;  
+
+        self.final_cube = np.clip((self.final_array / meantemp_cube), a_max=1)
+        """
+
 
     def auto_calibrate(self, img):
         pass
