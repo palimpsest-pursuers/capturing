a78a866b6c0c1898ce318d08f21bbfc3476c4453
keshav-sasanapuri <keshavsasanapuri@gmail.com>
2024-02-08 17:18:08 -0500

    added wheel file

---
+++ src/install spinnaker whl.py
@@ -0,0 +1,6 @@
+import pip
+
+def install_whl(path):
+    pip.main(['install', path])
+
+install_whl("edit-path\\capturing\\src\\spinnaker-whl file\\spinnaker_python-3.1.0.79-cp310-cp310-win_amd64.whl")
