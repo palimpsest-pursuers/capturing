fix lag in focus step by parallizing sharpness calculation in focus operation

src/controllers/blackfly_controller.py
@@ -5,8 +5,10 @@ try:
 except Exception as ex:
     print("Error:", ex)
     pass
-# import PySpin
+import time
 import numpy as np
+from threading import Thread
+import cv2
 
 
 '''
@@ -25,6 +27,7 @@ class BlackflyController(CameraInterface):
         self.selected_exposure_array = [self.ORIGINAL_EXPOSURE] * 16
         self.camera = None
         self.initialize_camera()
+        self.sharpness = 0
 
         try:
             if self.camera is not None:
@@ -127,23 +130,20 @@ class BlackflyController(CameraInterface):
 
             # Convert image to numpy array
             img_numpy = image_result.GetNDArray()
+
             # Rotate image by 180 degrees
             img_numpy = np.rot90(img_numpy, 2)
-            # Calculate Sharpness
-            img_normalized = (img_numpy - np.min(img_numpy)) / (np.max(img_numpy) - np.min(img_numpy))
-            # Calculate gradient
-            fx, fy = np.gradient(img_normalized * 255)
-            # Find maximum gradient
-            self.sharpness = np.max([np.max(fx), np.max(fy)])
-            print("sharpness: ", self.sharpness)
 
             # Release the image
             image_result.Release()
+
             return img_numpy
 
         except PySpin.SpinnakerException as ex:
             print("Error:", ex)
             return None
+        except Exception as ex:
+            print("Error:", ex)
 
     def capture_at_exposure(self, exposure, waveIndex):
         """
@@ -153,6 +153,7 @@ class BlackflyController(CameraInterface):
         """
         if self.change_exposure(exposure, waveIndex) == 0:
             return
+
         return self.capture()
 
     def change_exposure(self, change, waveIndex):
@@ -172,7 +173,7 @@ class BlackflyController(CameraInterface):
                 return 0
 
             self.camera.ExposureTime.SetValue(self.get_microseconds(new_exposure))
-            print("Exposure changed to: ", new_exposure, " when told to change by:", change)
+            # print("Exposure changed to: ", new_exposure, " when told to change by:", change)
 
         except PySpin.SpinnakerException as ex:
             print("Error:", ex)
@@ -224,19 +225,5 @@ class BlackflyController(CameraInterface):
         self.system.ReleaseInstance()
         print("camera un-initialized")
 
-    # def __del__(self):
-    #     """
-    #     Destructor to clean up resources when the object is destroyed.
-    #     :return: None
-    #     """
-    #     try:
-    #         if self.camera is not None:
-    #             self.camera.EndAcquisition()
-    #             self.camera.DeInit()
-    #         del self.camera
-    #         self.system.ReleaseInstance()
-    #     except PySpin.SpinnakerException as ex:
-    #         print('Error: %s' % ex)
-    #         return
 
 

src/controllers/pixilink_controller.py
@@ -20,7 +20,6 @@ Written by Cecelia Ahrens, and Robert Maron, Sai Keshav Sasanapuri
 class PixilinkController(CameraInterface):
     hCamera = None
     frame = None
-    # initialized = False
 
     '''Initialize the camera'''
 
@@ -53,9 +52,6 @@ class PixilinkController(CameraInterface):
     '''Function to Initialize camera'''
 
     def initialize_camera(self, waveIndex=0):
-        # if self.initialized:
-        #     print("Camera already Initialized")
-        #     return
         ret = PxLApi.initialize(0)
         if not (PxLApi.apiSuccess(ret[0])):
             print("Error: Unable to initialize a camera! rc = %i" % ret[0])
@@ -95,24 +91,20 @@ class PixilinkController(CameraInterface):
     '''Capture an image'''
 
     def capture(self):
+        """
+        Captures an image from the camera.
+        :return: Numpy array representing the captured image
+        """
         ret = self.get_next_frame(5)
+
         # frame was successful
         if PxLApi.apiSuccess(ret[0]):
-            # calculate sharpness
-            img_normalized = (self.frame - np.min(self.frame)) / (np.max(self.frame) - np.min(self.frame))
-            # Calculate gradient
-            fx, fy = np.gradient(img_normalized * 255)
-            # Find maximum gradient
-            self.sharpness = np.max([np.max(fx), np.max(fy)])
-            print("sharpness: ", self.sharpness)
-
             # update frame
             return self.frame
 
         # frame was unsuccessful
         else:
             print("Too many errors encountered, exiting")
-            # sys.exit(-1)
 
     """
     A robust wrapper around PxLApi.getNextFrame.
@@ -142,7 +134,7 @@ class PixilinkController(CameraInterface):
         print("tried")
         return ret
 
-    '''Capture an image at inital exposure multiplied by "exposure" '''
+    '''Capture an image at initial exposure multiplied by "exposure" '''
 
     def capture_at_exposure(self, exposure, waveIndex):
         if self.change_exposure(exposure, waveIndex) == 0:
@@ -193,10 +185,6 @@ class PixilinkController(CameraInterface):
     '''Un-initialize camera'''
 
     def uninitialize_camera(self):
-        # if not self.initialized:
-        #     print("Camera already Un-Initialized")
-        #     return
-
         # turn off stream state
         ret = PxLApi.setStreamState(self.hCamera, PxLApi.StreamState.STOP)
         assert PxLApi.apiSuccess(ret[0]), "setStreamState with StreamState.STOP failed"

src/operations/focus_operation.py
@@ -2,6 +2,10 @@ from operations.operation import Operation
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import time
+import numpy as np
+from threading import Thread
+import cv2
 
 '''
 Focus Operation for Displaying "Live" Images for Camara Focus Adjustment
@@ -66,17 +70,61 @@ class FocusWorker(QObject):
     notCancelled = True
     main = None
     finished = pyqtSignal()
+    
+    def calculate_sharpness(self, numpy_img, method):
+        start = time.time()
+        sharpness = 0
+        if method == 'gradient':
+            # Calculate Sharpness
+            # Gradient Method - OG Method
+            min_val = np.min(numpy_img)
+            max_val = np.max(numpy_img)
+            img_normalized = (numpy_img - min_val) / (max_val - min_val)
+
+            # Calculate gradient
+            fx, fy = np.gradient(img_normalized * 255)
+
+            sharpness = round(np.max([np.max(fx), np.max(fy)]))
+
+        elif method == 'laplacian':
+            # laplacian method
+            # The Laplacian operator detects edges by calculating the second derivative of the image intensity.
+            # The variance of the Laplacian gives a measure of focus—higher values indicate sharper images
+            laplacian = cv2.Laplacian(numpy_img, cv2.CV_64F)
+            variance = laplacian.var()
+
+            sharpness = round(variance)
+
+        elif method == 'brenner':
+            # Brenner gradient method
+            # This method uses differences between adjacent pixels to evaluate sharpness. It is simpler and faster
+            shifted = np.roll(numpy_img, -1, axis=1)
+            gradient = (numpy_img - shifted) ** 2
+            sharpness = round(np.sum(gradient)/1000000)
+
+        if self.notCancelled:
+            self.sharpness.emit(sharpness)
+
+        print("get sharpness time: ", time.time() - start)
 
     def run(self):
         self.main.led_control.turn_on(self.main.led_control.wavelength_list[8])
         # Initialize the camera
         self.main.camera_control.initialize_camera()
+        self.main.camera_control.change_exposure(self.main.camera_control.exposureArray[8], 8)
+        frame_no = -1
         while self.notCancelled:
-            frame = self.main.camera_control.capture_at_exposure(self.main.camera_control.exposureArray[8], 8)
+            full = time.time()
+            frame = self.main.camera_control.capture()
+            frame_no += 1
             img = self.main.camera_control.convert_nparray_to_QPixmap(frame)
             self.mainFrame.emit(img)
             self.x2Frame.emit(img)
             self.x4Frame.emit(img)
-            self.sharpness.emit(self.main.camera_control.get_sharpness())
+            if frame_no % 10 == 0:
+                Thread(target=self.calculate_sharpness, args=(frame, 'laplacian')).start()
+            # self.sharpness.emit(self.main.camera_control.get_sharpness())
+            print("Full time :", time.time() - full)
+
         self.main.camera_control.uninitialize_camera()
         self.finished.emit()